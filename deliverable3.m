9f037fcdaa090f2b4080439b6fc7a3ced32b9aad
JC Perrin <jcperrin@stanford.edu>
2018-04-12 00:24:09 -0700

    Decomposed into a helper function for each deliverable line item

---
+++ deliverable3.m
@@ -0,0 +1,11 @@
+function stateData = deliverable3(stateData, given, const)
+%% Deliverable 3
+% Construct performance-metric plots showing how specific thrust, thrust
+% specific fuel consumption, and thermal efficiency vary with spool speed.
+% Use the lower heating value of Jet A (42,800 kJ/kg) for your thermal
+% efficiency calculation, and base your performance metrics on the
+% calculated thrust.
+
+disp('Hello, world! 2');
+end
+
