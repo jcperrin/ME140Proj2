cfa8f15ffa54f969759be8f29eb2650eb42fb290
JC Perrin <jcperrin@stanford.edu>
2018-04-12 16:47:22 -0700

    Downloaded collected data

--- proj2Analysis.m
+++ proj2Analysis.m
@@ -17,9 +17,12 @@ clc;
 
 const.insqToMsq = 0.00064516; % [m^2/in^2]
 const.lbfToN = 4.44822; % [N/lbf]
-const.R = 287; % [kJ/kg/k] Gas cosntant of air
-const.KCdiff = 273; % [
+const.R = 287; % [kJ/kg/k] Gas constant of air
+const.KCdiff = 273; % [deg]
 const.kPaToPa = 1e3; % [Pa/kPa]
+const.kJkmol2Jkg = 1e3/28.97; %[J/kg * kmol/kJ]
+const.airCoefs = const.kJkmol2Jkg .* ...
+    fliplr([28.11, 0.1967e-2, 0.4802e-5,-1.966e-9]); % [J/kg/k] cp air
 
 %% Given Data
 % The following values were supplied to us in the original project
